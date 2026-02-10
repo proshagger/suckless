1d5ac2989668cfbb41818134f9b2da787b26fe0a
Hjolgram <gagi.gavrilo@gmail.com>
2026-02-10 17:07:22 +0100

    Initail files

---
+++ dmenu/config.h
@@ -0,0 +1,31 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c  option; centers dmenu on screen */
+static int min_width = 500;                 /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#d0d6d8", "#36454f" },
+	[SchemeSel] = { "#f2f4f5", "#36454f" },
+	[SchemeOut] = { "#000000", "#36454f" },
+};
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 0;
+static unsigned int columns    = 1;
+static unsigned int lineheight = 28;        /* -h  option; minimum height of a menu line */
+static unsigned int min_lineheight = 24;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 2;
